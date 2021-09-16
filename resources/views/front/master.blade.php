0e3956e42803863e1ecfef31801612c49bfabafd
AlbertWebs <albertmuhatia@gmail.com>
2021-09-16 11:02:18 +0300

    Theme

---
+++ resources/views/front/master.blade.php
@@ -0,0 +1,404 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	<meta name="robots" content="" />
+	<meta name="description" content="Industry - Factory & Industrial HTML Template" />
+	<meta property="og:title" content="Industry - Factory & Industrial HTML Template" />
+	<meta property="og:description" content="Industry - Factory & Industrial HTML Template" />
+	<meta property="og:image" content="" />
+	<meta name="format-detection" content="telephone=no">
+	
+	<!-- FAVICONS ICON -->
+	<link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png')}}" />
+	
+	<!-- PAGE TITLE HERE -->
+	<title>Apex Engineering - Factory & Industrial  | Designekta</title>
+	
+	<!-- MOBILE SPECIFIC -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.min.js"></script>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+	<link src="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+	
+	<!-- STYLESHEETS -->
+	<link rel="stylesheet" type="text/css" href="{{asset('theme/css/plugins.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('theme/plugins/fontawesome/css/font-awesome.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('theme/plugins/line-awesome/css/line-awesome.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('theme/plugins/flaticon/flaticon.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('theme/plugins/themify/themify-icons.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('theme/css/style.min.css')}}">
+	<link class="skin" rel="stylesheet" type="text/css" href="{{asset('theme/css/skin/skin-2.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('theme/css/templete.min.css')}}">
+	<!-- Google Font -->
+	<style>
+	@import url('https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i|Playfair+Display:400,400i,700,700i,900,900i|Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&amp;display=swap');
+	</style>
+	
+	<!-- REVOLUTION SLIDER CSS -->
+	<link rel="stylesheet" type="text/css" href="{{asset('theme/plugins/revolution/revolution/css/revolution.min.css')}}">
+
+</head>
+<body id="bg">
+<div class="page-wraper">
+<div id="loading-area"></div>
+    <!-- header -->
+    <header class="site-header mo-left header ext-header navstyle1">
+		<div class="top-bar">
+			<div class="container">
+				<div class="row d-flex justify-content-between align-items-center">
+					<div class="dlab-topbar-left">
+						<ul class="font-14">
+							<li><a href="#">About Us</a></li>
+							<li><a href="#">Refund Policy</a></li>
+							<li><a href="#">Help Desk</a></li>
+						</ul>
+					</div>
+					<div class="dlab-topbar-right">
+						<a download href="#" class="site-button radius-no btnhover11"><i class="ti-download"></i> &nbsp; Company Profile</a>						
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="middle-bar bg-white">
+			<div class="container">
+				<!-- website logo  -->
+				<div class="middle-area">
+					<div class="logo-header logo-dark">
+						<a href="{{url('/')}}"><img src="{{asset('theme/images/logo-2.png')}}" alt=""></a>
+					</div>
+					<div class="service-list">
+						<ul>
+							<li>
+								<i class="la la-phone"></i>
+								<h4 class="title">(+000)072.301.4032</h4>
+								<span>24Hours</span>
+							</li>
+							<li>
+								<i class="la la-clock-o"></i>
+								<h4 class="title">08:00 AM - 06:00 PM</h4>
+								<span>Monday - Friday</span>
+							</li>
+							<li>
+								<i class="la la-map"></i>
+								<h4 class="title">Buula Huubey, Wadajir District, </h4>
+								<span>Mogadishu Somalia.</span>
+							</li>
+						</ul>
+					</div>
+				</div>
+				
+			</div>
+		</div>	
+		<!-- main header -->
+        <div class="sticky-header main-bar-wraper navbar-expand-lg">
+            <div class="main-bar clearfix ">
+                <div class="container clearfix">
+                    <!-- website logo -->
+                    <div class="logo-header mostion logo-dark">
+						<a href="{{url('/')}}"><img src="{{asset('theme/images/logo-2.png')}}" alt=""></a>
+					</div>
+                    <!-- nav toggle button -->
+                    <button class="navbar-toggler collapsed navicon justify-content-end" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+						<span></span>
+						<span></span>
+						<span></span>
+					</button>
+                    <!-- extra nav -->
+                    <div class="extra-nav">
+                        <div class="extra-cell">
+                            <button id="quik-search-btn" type="button" class="site-button-link"><i class="la la-search"></i></button>
+						</div>
+                    </div>
+                    <!-- Quik search -->
+                    <div class="dlab-quik-search ">
+                        <form action="#">
+                            <input name="search" value="" type="text" class="form-control" placeholder="Type to search">
+                            <span id="quik-search-remove"><i class="ti-close"></i></span>
+                        </form>
+                    </div>
+                    <!-- main nav -->
+                    <div class="header-nav navbar-collapse collapse justify-content-start" id="navbarNavDropdown">
+						<div class="logo-header d-md-block d-lg-none">
+							<a href="{{url('/')}}"><img src="{{asset('theme/images/logo-2.png')}}" alt=""></a>
+						</div>
+                        <ul class="nav navbar-nav">	
+							<li>
+								<a href="javascript:;">Home <i style="font-size: 18px;" class="la la-home"></i> </a>
+								
+							</li>
+							<li>
+								<a href="javascript:;">About Us </a>
+								
+							</li>
+							<li>
+								<a href="javascript:;">The Company</i></a>
+								
+							</li>
+							<li class="active has-mega-menu homedemo"> <a href="javascript:;">Projects<i class="fa fa-chevron-down"></i></a>
+								<ul class="mega-menu">
+									<li><a href="{{url('/')}}"><img src="demo/pic1.jpg')}}" alt=""><span>Home - Default</span></a></li>
+									<li><a href="index-2.html"><img src="demo/pic2.jpg')}}" alt=""><span>Home - Oil/Gas Plant</span></a></li>
+									<li><a href="index-3.html"><img src="demo/pic3.jpg')}}" alt=""><span>Home - Steel Plant</span></a></li>
+									<li><a href="index-4.html"><img src="demo/pic4.jpg')}}" alt=""><span>Home - Factory</span></a></li>
+									<li><a href="index-5.html"><img src="demo/pic5.jpg')}}" alt=""><span>Home - Construct</span></a></li>
+									<li><a href="index-6.html"><img src="demo/pic6.jpg')}}" alt=""><span>Home - Solar Plant</span></a></li>
+									<li><a href="index-7.html"><img src="demo/pic7.jpg')}}" alt=""><span>Home - Food Industry</span></a></li>
+									<li><a href="index-8.html"><img src="demo/pic8.jpg')}}" alt=""><span>Home - Agriculture</span></a></li>
+									<li><a href="index-9.html"><img src="demo/pic9.jpg')}}" alt=""><span>Home - Ship Industry</span></a></li>
+									<li><a href="index-10.html"><img src="demo/pic10.jpg')}}" alt=""><span>Home - Leather Industry</span></a></li>
+									<li><a href="index-11.html"><img src="demo/pic11.jpg')}}" alt=""><span>Home - Nuclear Plant</span></a></li>
+									<li><a href="index-12.html"><img src="demo/pic12.jpg')}}" alt=""><span>Home - Beer Factory</span></a></li>
+									<li><a href="index-13.html"><img src="demo/pic13.jpg')}}" alt=""><span>Home - Mining Industry</span></a></li>
+									<li><a href="index-14.html"><img src="demo/pic14.jpg')}}" alt=""><span>Home - Car Industry</span></a></li>
+									<li><a href="index-15.html"><img src="demo/pic15.jpg')}}" alt=""><span>Home - Plastic Industry</span></a></li>
+								</ul>
+							</li>
+
+							<li>
+								<a href="javascript:;">Services </a>
+								
+							</li>
+
+							<li>
+								<a href="javascript:;">Latest News </a>
+								
+							</li>
+						
+							<li>
+								<a href="javascript:;">Contact Us &nbsp; <i style="font-size: 18px;" class="la la-phone"></i></a>
+								
+							</li>
+						</ul>	
+						<div class="dlab-social-icon">
+							<ul>
+								<li><a class="site-button facebook sharp-sm outline fa fa-facebook" href="javascript:void(0);"></a></li>
+								<li><a class="site-button twitter sharp-sm outline fa fa-twitter" href="javascript:void(0);"></a></li>
+								<li><a class="site-button linkedin sharp-sm outline fa fa-linkedin" href="javascript:void(0);"></a></li>
+								<li><a class="site-button instagram sharp-sm outline fa fa-instagram" href="javascript:void(0);"></a></li>
+							</ul>
+						</div>	
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- main header END -->
+    </header>
+    <!-- header END -->
+
+    @yield('content')
+
+	<!-- Footer -->
+    <footer class="site-footer style2">
+		<!-- Footer Info -->
+		<div class="section-full p-t50 p-b20 bg-primary text-white overlay-primary-dark footer-info-bar wow fadeIn" data-wow-duration="2s" data-wow-delay="0.6s">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 col-md-6 col-sm-6 m-b30">
+						<div class="icon-bx-wraper bx-style-1 p-a20 radius-sm br-col-w1">
+							<div class="icon-content">
+								<h5 class="dlab-tilte">
+									<span class="icon-sm"><i class="ti-location-pin"></i></span> 
+									Company Address
+								</h5>
+								<p class="op7"> demo address #8901 Marmora Road Chi Minh City, Somalia</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 m-b30">
+						<div class="icon-bx-wraper bx-style-1 p-a20 radius-sm br-col-w1">
+							<div class="icon-content">
+								<h5 class="dlab-tilte">
+									<span class="icon-sm"><i class="ti-email"></i></span> 
+									E-mail
+								</h5>
+								<p class="m-b0 op7">info@apexengltd.com</p>
+								<p class="op7">hello@apexengltd.com</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 m-b30">
+						<div class="icon-bx-wraper bx-style-1 p-a20 radius-sm br-col-w1">
+							<div class="icon-content">
+								<h5 class="dlab-tilte">
+									<span class="icon-sm"><i class="ti-mobile"></i></span> 
+									Phone Numbers
+								</h5>
+								<p class="m-b0 op7">Mobile : +00 234 678 9012</p>
+								<p class="op7">Phone : +0 1234 5678 90</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 m-b30">
+						<div class="icon-bx-wraper bx-style-1 p-a20 radius-sm br-col-w1">
+							<div class="icon-content">
+								<h5 class="dlab-tilte">
+									<span class="icon-sm"><i class="ti-alarm-clock"></i></span> 
+									Office Hours
+								</h5>
+								<p class="m-b0 op7">Mon To Sat - 08.00-18.00</p>
+								<p class="op7">Sunday - Close</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Footer Info End -->
+        <!-- Footer Top -->
+		<div class="footer-top">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-4 col-md-12 col-sm-12">
+                        <div class="widget widget_about">
+							<div class="footer-logo logo-dark">
+								<a href="{{url('/')}}"><img src="{{asset('theme/images/logo.png')}}" alt=""/></a>
+							</div>
+							<p>Industry Lorem Ipsum has been the industry's standard dummy text ever since the when an printer took a galley of type and scrambled it to make.</p>
+							<div class="widget widget_getintuch">
+								<ul>
+									<li><i class="ti-location-pin"></i> demo address #8901 Marmora Road Chi Minh City, Somalia </li>
+									<li><i class="ti-mobile"></i> 0800-123456 (24/7 Support Line)</li>
+									<li><i class="ti-email"></i> info@apexeng.com</li>
+								</ul>
+							</div>
+							<ul class="list-inline m-a0">
+								<li><a href="javascript:void(0);" class="site-button-link facebook button-md"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="javascript:void(0);" class="site-button-link google-plus button-md"><i class="fa fa-google-plus"></i></a></li>
+								<li><a href="javascript:void(0);" class="site-button-link linkedin button-md"><i class="fa fa-linkedin"></i></a></li>
+								<li><a href="javascript:void(0);" class="site-button-link instagram button-md"><i class="fa fa-instagram"></i></a></li>
+								<li><a href="javascript:void(0);" class="site-button-link twitter button-md"><i class="fa fa-twitter"></i></a></li>
+							</ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12">
+                        <div class="widget">
+                            <h4 class="footer-title">Usefull Link</h4>
+                            <ul class="list-2">
+                                <li><a href="about-1.html">About Us</a></li>
+                                <li><a href="blog-grid-2.html">Blog</a></li>
+                                <li><a href="services-1.html">Services</a></li>
+                                <li><a href="privacy-policy.html">Privacy Policy</a></li>
+                                <li><a href="portfolio-details.html">Projects </a></li>
+                                <li><a href="help-desk.html">Help Desk </a></li>
+                                <li><a href="shop.html">Shop</a></li>
+                                <li><a href="contact-1.html">Contact</a></li>
+                            </ul>							
+                        </div>
+						<div class="widget">
+                            <h4 class="footer-title mb-2">Newsletter</h4>
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+							<div class="subscribe-form m-b20 m-t30">
+								<form class="dzSubscribe" action="https://industry.dexignzone.com/xhtml/script/mailchamp.php" method="post">
+									<div class="dzSubscribeMsg"></div>
+									<div class="input-group">
+										<input name="dzEmail" required="required" class="form-control radius-no" placeholder="Your Email Address" type="email">
+										<span class="input-group-btn">
+											<button name="submit" value="Submit" type="submit" class="site-button radius-no">SEND</button>
+										</span> 
+									</div>
+								</form>
+							</div>
+						</div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12">
+                        <div class="widget recent-posts-entry">
+                            <h4 class="footer-title">Recent Post</h4>
+							<div class="widget-post-bx">
+								<div class="widget-post clearfix">
+									<div class="dlab-post-media"> 
+										<img src="{{asset('theme/images/blog/recent-blog/pic1.jpg')}}" width="200" height="143" alt=""> 
+									</div>
+									<div class="dlab-post-info">
+										<div class="dlab-post-meta">
+											<ul>
+												<li class="post-date"> <strong>13 Aug</strong> </li>
+												<li class="post-author"> By <a href="javascript:void(0);">Mohamed </a> </li>
+											</ul>
+										</div>
+										<div class="dlab-post-header">
+											<h6 class="post-title"><a href="blog-single-left-sidebar.html">How To Get People To Like Industry</a></h6>
+										</div>
+									</div>
+								</div>
+								<div class="widget-post clearfix">
+									<div class="dlab-post-media"> 
+										<img src="{{asset('theme/images/blog/recent-blog/pic2.jpg')}}" width="200" height="160" alt=""> 
+									</div>
+									<div class="dlab-post-info">
+										<div class="dlab-post-meta">
+											<ul>
+												<li class="post-date"> <strong>13 Aug</strong> </li>
+												<li class="post-author"> By <a href="javascript:void(0);">Mohamed </a> </li>
+											</ul>
+										</div>
+										<div class="dlab-post-header">
+											<h6 class="post-title"><a href="blog-single-left-sidebar.html">Seven Doubts You Should Clarify About</a></h6>
+										</div>
+									</div>
+								</div>
+								<div class="widget-post clearfix">
+									<div class="dlab-post-media"> 
+										<img src="{{asset('theme/images/blog/recent-blog/pic3.jpg')}}" width="200" height="160" alt=""> 
+									</div>
+									<div class="dlab-post-info">
+										<div class="dlab-post-meta">
+											<ul>
+												<li class="post-date"> <strong>13 Aug</strong> </li>
+												<li class="post-author"> By <a href="javascript:void(0);">Mohamed </a> </li>
+											</ul>
+										</div>
+										<div class="dlab-post-header">
+											<h6 class="post-title"><a href="blog-single-left-sidebar.html">Why You Should Not Go To Industry</a></h6>
+										</div>
+									</div>
+								</div>
+							</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+		<!-- Footer Top End -->
+    </footer>
+    <!-- Footer End -->
+    <button class="scroltop style1 radius" type="button"><i class="fa fa-arrow-up"></i></button>
+</div>
+<!-- JAVASCRIPT FILES ========================================= -->
+ <script src="{{asset('theme/js/combining.js')}}"></script>
+ <!-- CONTACT JS  -->
+ <script src="{{asset('theme/js/jquery.lazy.min.js')}}"></script>
+ <!-- REVOLUTION JS FILES -->
+ <script src="{{asset('theme/plugins/revolution/revolution/js/jquery.themepunch.tools.min.js')}}"></script>
+ <script src="{{asset('theme/plugins/revolution/revolution/js/jquery.themepunch.revolution.min.js')}}"></script>
+ <script src="{{asset('theme/js/rev.slider.js')}}"></script>
+
+ <!-- Hero Video -->
+ <script type="text/javascript" src="{{asset('theme/plugins/revolution/revolution/js/extensions/revolution.extension.video.min.js')}}"></script>
+ <script type="text/javascript" src="{{asset('theme/plugins/revolution/revolution/js/extensions/revolution.extension.slideanims.min.js')}}"></script>
+ <script type="text/javascript" src="{{asset('theme/plugins/revolution/revolution/js/extensions/revolution.extension.actions.min.js')}}"></script>
+ <script type="text/javascript" src="{{asset('theme/plugins/revolution/revolution/js/extensions/revolution.extension.layeranimation.min.js')}}"></script>
+ <script type="text/javascript" src="{{asset('theme/plugins/revolution/revolution/js/extensions/revolution.extension.kenburn.min.js')}}"></script>
+ <script type="text/javascript" src="{{asset('theme/plugins/revolution/revolution/js/extensions/revolution.extension.navigation.min.js')}}"></script>
+ <script type="text/javascript" src="{{asset('theme/plugins/revolution/revolution/js/extensions/revolution.extension.parallax.min.js')}}"></script>
+ <!-- Hero Video -->
+
+
+ <script>
+	jQuery(document).ready(function() {
+	   'use strict';
+	   dz_rev_slider_1();	
+	   $('.lazy').Lazy();
+	});	/*ready*/
+ </script>
+</body>
+
+</html>
