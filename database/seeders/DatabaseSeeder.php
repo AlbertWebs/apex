526cf4f597493e865ce68cc1ec54ca6875e1c33a
AlbertWebs <albertmuhatia@gmail.com>
2021-09-25 22:18:48 +0300

    admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = [
+            [
+               'name'=>'Admin',
+               'email'=>'admin@designekta.com',
+                'is_admin'=>'1',
+               'password'=> bcrypt('123456'),
+            ],
+            [
+               'name'=>'User',
+               'email'=>'user@designekta.com',
+                'is_admin'=>'0',
+               'password'=> bcrypt('123456'),
+            ],
+        ];
+  
+        foreach ($user as $key => $value) {
+            User::create($value);
+        }
     }
 }
