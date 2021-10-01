d4dec677d5f6f73099d6cb9da1d20032d987b06b
AlbertWebs <albertmuhatia@gmail.com>
2021-10-01 11:21:16 +0300

    porfolios pages

--- resources/views/front/portfolios.blade.php
+++ resources/views/front/portfolios.blade.php
@@ -6,12 +6,12 @@
             <div class="dlab-bnr-inr overlay-black-middle bg-pt" style="background-image:url({{asset('theme/images/banner/architecture-2287327_1920.jpg')}});">
                 <div class="container">
                     <div class="dlab-bnr-inr-entry">
-                        <h1 class="text-white"> Services </h1>
+                        <h1 class="text-white"> Our Projects </h1>
                         <!-- Breadcrumb row -->
                         <div class="breadcrumb-row">
                             <ul class="list-inline">
                                 <li><a href="{{url('/')}}">Home</a></li>
-                                <li>Services </li>
+                                <li>Our Projects </li>
                             </ul>
                         </div>
                         <!-- Breadcrumb row END -->
