78a7d8ba7d4d963ab7f4cb13e726638ce1dc9c54
AlbertWebs <albertmuhatia@gmail.com>
2021-10-25 09:23:14 +0300

    ProService

--- resources/views/schema.blade.php
+++ resources/views/schema.blade.php
@@ -1,7 +1,7 @@
 <script type='application/ld+json'>
     {
       "@context": "http://www.schema.org",
-      "@type": "WebPage",
+      "@type": "ProfessionalService",
       "name": "Apex Engineering Ltd",
       "url": "http://apexengltd.com/",
       "logo": "https://www.apexengltd.com/theme/images/logo-2.png",
